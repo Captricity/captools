e0dfc2983347b4f12927367f1317a48d03ffcb29
Yoriyasu Yano <yoriy@captricity.com>
2013-11-12 15:13:20 -0800

    Update readme and bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,44 @@
 
 from distutils.core import setup
 
-LONG_DESCRIPTION = open('README.md', 'r').read()
+LONG_DESCRIPTION = '''
+Captricity API Python Client
+----------------------------
+
+This is a Python client for the Captricity API
+
+Pip Installation of the Captricity Python Client
+------------------------------------------------
+
+If you use the pip package manager, the following will install the Captricity API Client:
+    
+    pip install captricity-python-client
+
+Manual Installation of the Captricity Python Client
+---------------------------------------------------
+
+`Download the zipped package <https://github.com/Captricity/captools/zipball/master>` or clone the repository using git if you haven't done so already:
+
+    git clone git@github.com:Captricity/captools.git
+
+captools comes with a setup.py, which makes it easy to install into your python environment:
+    
+    cd captools
+    python setup.py install
+
+You should now be able to import captools.api in your python environment.
+
+Quickstart
+----------
+
+Please see the `Captricity API Quickstart guide <https://shreddr.captricity.com/developer/quickstart/>` for a short hands-on introduction to using this client with the Captricity API.  
+
+License
+-------
+This is licensed under the MIT license. See LICENSE.txt.'''
 
 setup(name='captricity-python-client',
-        version='0.18',
+        version='0.19',
         description='Python client to access Captricity API',
         url='https://github.com/Captricity/captools',
         author='Captricity, Inc',
